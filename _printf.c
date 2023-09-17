4c62a82ab17f85ac9adee33499ced35556b19e51
munene748 <muneneemmanuel22@gmail.com>
2023-09-17 22:19:17 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -1,66 +1,89 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h> /* For sprintf */
+
+#define BUFF_SIZE 1024
 
 /**
- * _printf - produces output according to a format
- * @format: string
- * Return: number of characters printed (excluding the null byte)
+ * print_buffer - Prints buffer contents.
+ * @buffer: Array of characters.
+ * @buff_ind: Index for the next character, resets to 0.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
+}
+
+/**
+ * _printf - Custom printf function.
+ * @format: Format string.
+ *
+ * Return: Number of characters printed (excluding null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int char_count = 0; /* Number of characters printed */
+	int i, char_count = 0;
+	int buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
-	va_start(args, format);
+	va_start(list, format);
 
-	while (*format)
+	for (i = 0; format[i]; i++)
 	{
-		if (*format == '%')
+		if (format[i] != '%')
 		{
-			format++;
-
-			if (*format == '\0')
-				break;
-
-			if (*format == 'c')
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			char_count++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			i++;
+			if (format[i] == 'c')
 			{
-				int c = va_arg(args, int);
-				write(1, &c, 1);
+				int c = va_arg(list, int);
+				buffer[buff_ind++] = c;
 				char_count++;
 			}
-			else if (*format == 's')
+			else if (format[i] == 's')
 			{
-				char *s = va_arg(args, char *);
-				if (s != NULL)
+				char *s = va_arg(list, char *);
+				if (s)
 				{
 					while (*s)
 					{
-						write(1, s, 1);
-						s++;
+						buffer[buff_ind++] = *s++;
 						char_count++;
 					}
 				}
 			}
-			else if (*format == '%')
+			else if (format[i] == 'd' || format[i] == 'u')
 			{
-				write(1, "%", 1);
-				char_count++;
+				char num_str[20];
+				int num = va_arg(list, int);
+				sprintf(num_str, (format[i] == 'd') ? "%d" : "%u", num);
+				int j;
+				for (j = 0; num_str[j]; j++)
+				{
+					buffer[buff_ind++] = num_str[j];
+					char_count++;
+				}
 			}
 		}
-		else
-		{
-			write(1, format, 1);
-			char_count++;
-		}
-
-		format++;
 	}
 
-	va_end(args);
+	print_buffer(buffer, &buff_ind);
+	va_end(list);
+
 	return (char_count);
 }
 
