cb558f8d46a4e59bb78f14c74a54933434c146d7
munene748 <muneneemmanuel22@gmail.com>
2023-09-17 04:10:44 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -1,67 +1,76 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
 /**
  * _printf - produces output according to a format
  * @format: string
- * Return: printed chars
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-
 	va_list arg;
 	int i = 0;
+	int printed_chars = 0;
 
 	if (format == NULL)
-                return (-1);
+		return (-1);
 
 	va_start(arg, format);
-	
+
 	while (format[i])
-        {
+	{
 		if (format[i] == '%')
 		{
 			i++;
-		 if (format[i] == '\0')
-                 break;
+			if (format[i] == '\0')
+				break;
 
-		 switch (format[i])
-		 {
+			switch (format[i])
+			{
+				case 'c':
+				{
+					int c = va_arg(arg, int);
+					write(1, &c, 1);
+					printed_chars++;
+					break;
+				}
 
-		case 'c':
-		{
-		 int c = va_arg(arg, int);
-                 write(1, &c, 1);
-                 break;
-		}
+				case 's':
+				{
+					char *s1 = va_arg(arg, char *);
+					if (s1 != NULL)
+					{
+						while (*s1)
+						{
+							write(1, s1, 1);
+							s1++;
+							printed_chars++;
+						}
+					}
+					break;
+				}
 
-		case 's':
-		{
-			char *s1 = va_arg(arg, char *);
-                  if (s1 != NULL)
-                    {
-                        while (*s1)
-                        {
-                            write(1, s1, 1);
-                            s1++;
-                        }
-                    }
-                    break;
-                 }
+				case '%':
+					write(1, &format[i], 1);
+					printed_chars++;
+					break;
 
-		case '%':
-		write(1, &format[i], 1);
-                break;
-
-		default:
-                write(1, &format[i], 1);
-                break;
-		 }
+				default:
+					write(1, &format[i], 1);
+					printed_chars++;
+					break;
+			}
 		}
 		else
 		{
 			write(1, &format[i], 1);
+			printed_chars++;
 		}
 		i++;
 	}
+
 	va_end(arg);
-	return (i);
+	return (printed_chars);
 }
+
