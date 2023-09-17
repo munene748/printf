b0a60df428fef8371aa7c81dbd1332c9b0b40ee2
munene748 <muneneemmanuel22@gmail.com>
2023-09-17 14:12:14 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -9,85 +9,80 @@
  */
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int i = 0;
-	int printed_chars = 0;
+    va_list arg;
+    int i = 0;
+    int printed_chars = 0;
 
-	if (format == NULL)
-<<<<<<< HEAD
-	return (-1);
-=======
-		return (-1);
->>>>>>> cb558f8d46a4e59bb78f14c74a54933434c146d7
+    if (format == NULL)
+        return (-1);
 
-	va_start(arg, format);
+    va_start(arg, format);
 
-	while (format[i])
-	{
-<<<<<<< HEAD
-	if (format[i] == '%')
-	{
-	i++;
-	       	if (format[i] == '\0')
-                 break;
-=======
-		if (format[i] == '%')
-		{
-			i++;
-			if (format[i] == '\0')
-				break;
->>>>>>> cb558f8d46a4e59bb78f14c74a54933434c146d7
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == '\0')
+                break;
+            if (format[i] == '%')
+            {
+                i++;
+                if (format[i] == '\0')
+                    break;
 
-			switch (format[i])
-			{
-				case 'c':
-				{
-					int c = va_arg(arg, int);
-					write(1, &c, 1);
-					printed_chars++;
-					break;
-				}
+                switch (format[i])
+                {
+                    case 'c':
+                    {
+                        int c = va_arg(arg, int);
+                        write(1, &c, 1);
+                        printed_chars++;
+                        break;
+                    }
 
-				case 's':
-				{
-					char *s1 = va_arg(arg, char *);
-					if (s1 != NULL)
-					{
-						while (*s1)
-						{
-							write(1, s1, 1);
-							s1++;
-							printed_chars++;
-						}
-					}
-					break;
-				}
+                    case 's':
+                    {
+                        char *s1 = va_arg(arg, char *);
+                        if (s1 != NULL)
+                        {
+                            while (*s1)
+                            {
+                                write(1, s1, 1);
+                                s1++;
+                                printed_chars++;
+                            }
+                        }
+                        break;
+                    }
 
-				case '%':
-					write(1, &format[i], 1);
-					printed_chars++;
-					break;
+                    case '%':
+                        write(1, &format[i], 1);
+                        printed_chars++;
+                        break;
 
-				default:
-					write(1, &format[i], 1);
-					printed_chars++;
-					break;
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			printed_chars++;
-		}
-		i++;
-	}
+                    default:
+                        write(1, &format[i], 1);
+                        printed_chars++;
+                        break;
+                }
+            }
+            else
+            {
+                write(1, &format[i], 1);
+                printed_chars++;
+            }
+            i++;
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            printed_chars++;
+            i++;
+        }
+    }
 
-	va_end(arg);
-<<<<<<< HEAD
-
-	return (i);
-=======
-	return (printed_chars);
->>>>>>> cb558f8d46a4e59bb78f14c74a54933434c146d7
+    va_end(arg);
+    return (printed_chars);
 }
 
