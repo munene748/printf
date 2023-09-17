8d7da281f7fcb604d59f39559c833cca791a3f69
munene748 <muneneemmanuel22@gmail.com>
2023-09-17 16:35:29 +0300

    printf

--- _printf.c
+++ _printf.c
@@ -5,117 +5,62 @@
 /**
  * _printf - produces output according to a format
  * @format: string
- * Return: number of characters printed
+ * Return: number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-    va_list arg;
-    int i = 0;
-    int printed_chars = 0;
+	va_list args;
+	int char_count = 0; /* Number of characters printed */
 
-<<<<<<< HEAD
-    if (format == NULL)
-        return (-1);
-=======
 	if (format == NULL)
-	return (-1);
+		return (-1);
 
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
+	va_start(args, format);
 
-    va_start(arg, format);
-
-<<<<<<< HEAD
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                break;
-            if (format[i] == '%')
-            {
-                i++;
-                if (format[i] == '\0')
-                    break;
-=======
-	while (format[i])
+	while (*format)
 	{
-
-	if (format[i] == '%')
-	{
-	i++;
-	       	if (format[i] == '\0')
-                 break;
-
-		if (format[i] == '%')
+		if (*format == '%')
 		{
-			i++;
-			if (format[i] == '\0')
+			format++;
+
+			if (*format == '\0')
 				break;
 
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
+			if (*format == 'c')
+			{
+				int c = va_arg(args, int);
+				write(1, &c, 1);
+				char_count++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(args, char *);
+				if (s != NULL)
+				{
+					while (*s)
+					{
+						write(1, s, 1);
+						s++;
+						char_count++;
+					}
+				}
+			}
+			else if (*format == '%')
+			{
+				write(1, "%", 1);
+				char_count++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			char_count++;
+		}
 
-                switch (format[i])
-                {
-                    case 'c':
-                    {
-                        int c = va_arg(arg, int);
-                        write(1, &c, 1);
-                        printed_chars++;
-                        break;
-                    }
+		format++;
+	}
 
-                    case 's':
-                    {
-                        char *s1 = va_arg(arg, char *);
-                        if (s1 != NULL)
-                        {
-                            while (*s1)
-                            {
-                                write(1, s1, 1);
-                                s1++;
-                                printed_chars++;
-                            }
-                        }
-                        break;
-                    }
-
-                    case '%':
-                        write(1, &format[i], 1);
-                        printed_chars++;
-                        break;
-
-                    default:
-                        write(1, &format[i], 1);
-                        printed_chars++;
-                        break;
-                }
-            }
-            else
-            {
-                write(1, &format[i], 1);
-                printed_chars++;
-            }
-            i++;
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            printed_chars++;
-            i++;
-        }
-    }
-
-<<<<<<< HEAD
-    va_end(arg);
-    return (printed_chars);
+	va_end(args);
+	return (char_count);
 }
-=======
-	va_end(arg);
 
-
-	return (i);
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
-
-	return (printed_chars);
-}
--- main.c
+++ main.c
@@ -1,49 +1,23 @@
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
-<<<<<<< HEAD
-=======
 
 /**
  * main - Entry point
  *
  * Return: Always 0
  */
-
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
-#include <stdint.h>
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
 int main(void)
 {
     int len;
     int len2;
     unsigned int ui;
-<<<<<<< HEAD
-    char *addr_str;
-=======
     void *addr;
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
-<<<<<<< HEAD
-=======
-    addr = (void *)0x7ffe637541f0;
-
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
-
-    addr_str = "0x7ffe637541f0";
-
-<<<<<<< HEAD
-=======
-    /* Cast uintptr_t to void* */
-    (void)(uintptr_t)0x7ffe6375;
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
+    addr = (void *)0x7FFF;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -58,30 +32,14 @@ int main(void)
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-<<<<<<< HEAD
-    _printf("Address:[%s]\n", addr_str);
-    printf("Address:[%s]\n", addr_str);
-=======
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-<<<<<<< HEAD
-=======
-
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    _printf("Unknown:[%%r]\n");
+    printf("Unknown:[%%r]\n");
     return (0);
 }
-<<<<<<< HEAD
 
-=======
-
-
-    return (0);
-}
->>>>>>> 0cbc678c2b0ec828dd3c015264fa9ec9b4be6cc5
