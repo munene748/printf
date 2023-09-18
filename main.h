31af1c4ed71e8f31ed5aa7b3de82224a1df4536d
hopecarla <hopememba.carla@gmail.com>
2023-09-18 08:21:10 -0700

    printf

--- main.h
+++ main.h
@@ -1,16 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stddef.h>
 #include <limits.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-/* Macros for buffer size */
-#define BUFFER_SIZE 1024
-
-/* Macros for flag characters */
+/* Flag Modifier Macros */
 #define PLUS 1
 #define SPACE 2
 #define HASH 4
@@ -22,7 +18,7 @@
 #define ZERO_FLAG ((flags >> 3) & 1)
 #define NEG_FLAG ((flags >> 4) & 1)
 
-/* Macros for length modifiers */
+/* Length Modifier Macros */
 #define SHORT 1
 #define LONG 2
 
@@ -34,15 +30,27 @@
  */
 typedef struct buffer_s
 {
-    char *buffer;
-    char *start;
-    unsigned int len;
+	char *buffer;
+	char *start;
+	unsigned int len;
 } buffer_t;
+
+/**
+ * struct converter_s - Defines a converter's structure.
+ * @specifier: A character representing a conversion specifier.
+ * @func: A pointer to a conversion function corresponding to specifier.
+ */
+typedef struct converter_s
+{
+	unsigned char specifier;
+	unsigned int (*func)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+} converter_t;
 
 /* Main Custom Printf Function */
 int _printf(const char *format, ...);
 
-/* Converters */
+/* Converters  */
 unsigned int convert_char(va_list args, buffer_t *output,
 unsigned char flags, int width, int precision, unsigned char length);
 unsigned int convert_str(va_list args, buffer_t *output,
@@ -74,7 +82,8 @@ unsigned char flags, int width, int precision, unsigned char length);
 unsigned char handle_flags(const char *flags_str, char *index);
 unsigned char handle_length(const char *length_modifier, char *index);
 int handle_width(va_list args, const char *width_modifier, char *index);
-int handle_precision(va_list args, const char *precision_modifier, char *index);
+int handle_precision(va_list args, const char
+*precision_modifier, char *index);
 unsigned int (*handle_specifiers(const char
 *specifier))(va_list, buffer_t *,
 unsigned char, int, int, unsigned char);
@@ -97,8 +106,5 @@ unsigned int convert_unsignedBase(buffer_t *output,
 unsigned long int number, char *base, unsigned char flags,
 int width, int precision);
 
-
-
-
-#endif /* MAIN_H */
+#endif
 
