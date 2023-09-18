5e4a6c15b87e4dd33a941d0acc203ab0219b4110
hopecarla <hopememba.carla@gmail.com>
2023-09-18 07:07:25 -0700

    printf

--- main.h
+++ main.h
@@ -75,6 +75,30 @@ unsigned char handle_flags(const char *flags_str, char *index);
 unsigned char handle_length(const char *length_modifier, char *index);
 int handle_width(va_list args, const char *width_modifier, char *index);
 int handle_precision(va_list args, const char *precision_modifier, char *index);
+unsigned int (*handle_specifiers(const char
+*specifier))(va_list, buffer_t *,
+unsigned char, int, int, unsigned char);
+
+/* Modifiers */
+unsigned int print_width(buffer_t *output, unsigned int printed_count,
+unsigned char flags, int width);
+unsigned int print_string_width(buffer_t *output, unsigned char flags,
+int width, int precision, int string_size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed_count,
+unsigned char flags, int width);
+
+/* Helper */
+buffer_t *init_buffer(void);
+void free_buffer(buffer_t *output);
+unsigned int _memcpy(buffer_t *output, const char *src, unsigned int n);
+unsigned int convert_signedBase(buffer_t *output, long int number, char *base,
+unsigned char flags, int width, int precision);
+unsigned int convert_unsignedBase(buffer_t *output,
+unsigned long int number, char *base, unsigned char flags,
+int width, int precision);
+
+
+
 
 #endif /* MAIN_H */
 
